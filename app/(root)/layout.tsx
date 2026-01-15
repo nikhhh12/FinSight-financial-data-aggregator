bfaa1356486bb88dc8ba63130905e499b50a28f7
nikhhh12 <nikkhil.theguru@gmail.com>
2026-01-15 15:35:58 +0530

    Implement Auth UI & Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,22 +1,22 @@
 import Header from "@/components/Header";
-// import {auth} from "@/lib/better-auth/auth";
+import {auth} from "@/lib/better-auth/auth";
 import {headers} from "next/headers";
-// import {redirect} from "next/navigation";
+import {redirect} from "next/navigation";
 
 const Layout = async ({ children }: { children : React.ReactNode }) => {
-    // const session = await auth.api.getSession({ headers: await headers() });
+    const session = await auth.api.getSession({ headers: await headers() });
 
-    // if(!session?.user) redirect('/sign-in');
+    if(!session?.user) redirect('/sign-in');
 
-    // const user = {
-    //     id: session.user.id,
-    //     name: session.user.name,
-    //     email: session.user.email,
-    // }
+    const user = {
+        id: session.user.id,
+        name: session.user.name,
+        email: session.user.email,
+    }
 
     return (
         <main className="min-h-screen text-gray-400">
-            {/* <Header user={user} /> */}
+            <Header user={user} />
 
             <div className="container py-10">
                 {children}
