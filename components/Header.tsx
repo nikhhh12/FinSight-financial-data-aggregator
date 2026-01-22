41e4ee5858e799a1db1e79100af1f6494efebc20
nikhhh12 <nikkhil.theguru@gmail.com>
2026-01-22 17:00:49 +0530

    implement database setup

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/components/NavItems";
 import UserDropdown from "@/components/UserDropdown";
-import {searchStocks} from "@/lib/actions/finnhub.actions";
+// import {searchStocks} from "@/lib/actions/finnhub.actions";
 
 const Header = async ({ user }: { user: User }) => {
     const initialStocks = await searchStocks();
